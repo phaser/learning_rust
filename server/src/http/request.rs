0a78181772362a99ae374c7c171dc440c9ba0b4b
Cristian Bidea <cristian.bidea@uipath.com>
2025-04-24 13:54:32 +0300

    Decoding from utf8

--- server/src/http/request.rs
+++ server/src/http/request.rs
@@ -1,7 +1,64 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Display, Debug, Result as FmtResult, Formatter};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
     query_string: Option<String>,
     method: Method
+}
+
+impl Request {
+}
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {        
+        let request = str::from_utf8(buf)?;
+
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEnconding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid Request",
+            Self::InvalidEnconding => "Invalid Encoding",
+            Self::InvalidProtocol => "Invalid Protocol",
+            Self::InvalidMethod => "Invalid Method"
+        }
+    }
+}
+
+impl Error for ParseError {
+
+}
+
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEnconding
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult { 
+        write!(f, "{}", self.message())    
+    }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,5 +1,8 @@
 use std::io::Read;
 use std::net::TcpListener;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     addr: String,
@@ -24,6 +27,10 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Error on decoding request: {}", e)
+                            }
                         },
                         Err(e) => println!("Failed to read from connection: {}", e)
                     }
