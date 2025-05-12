497208466f654351d13f1d17a311208081a9794b
Cristian Bidea <cristian.bidea@uipath.com>
2025-05-12 13:40:40 +0300

    New project

---
+++ cards_deck/src/main.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        Deck { cards }
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
+    println!("Heres your deck: {:#?}", deck);
+}
