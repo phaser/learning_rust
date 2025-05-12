5d7041c71c1966f83c018312368e48189752c5a0
Cristian Bidea <cristian.bidea@uipath.com>
2025-05-12 19:31:51 +0300

    tmp

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,3 +1,34 @@
+#[derive(Debug)]
+struct Account {
+    id: u64,
+    balance: i64,
+    holder: String
+}
+
+impl Account {
+    fn new(id: u64, holder: String) -> Self {
+        Account { id, holder, balance: 0 }
+    }
+}
+
+#[derive(Debug)]
+struct Bank {
+    account: Vec<Account>
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { account: vec![] }
+    }
+}
+
+fn print_account(account: &Account) {
+    println!("{:#?}", account);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+
+    let other_bank = &bank;
+    println!("{:#?}", other_bank);
 }
