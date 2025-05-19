c7fb30a218165181e72dac4b8e7d532f2acf9fa7
Cristian Bidea <cristian.bidea@uipath.com>
2025-05-19 19:44:18 +0300

    Minimal endpoints for setting and getting values

--- toydb/src/database.rs
+++ toydb/src/database.rs
@@ -8,10 +8,16 @@ pub struct Database {
 
 impl Database {
     pub fn new(path: String) -> Self {
-        Self {
-            hash_map: HashMap::new(),
-            storage: Storage::new(path),
+        let mut hash_map = HashMap::new();
+        let storage = Storage::new(path);
+        let mut storage_file = storage.get_mut_file();
+        let mut next_record = storage.get_key_value(&mut storage_file);
+        while next_record.is_some() {
+            let (key, value) = next_record.unwrap();
+            hash_map.insert(key, value);
+            next_record = storage.get_key_value(&mut storage_file);
         }
+        Database { hash_map, storage }
     }
 
     pub fn put(&mut self, key: String, value: String) {
@@ -19,8 +25,8 @@ impl Database {
         self.hash_map.insert(key, value);
     }
 
-    pub fn get(&self, key: String) -> Option<String> {
-        self.hash_map.get(&key).cloned()
+    pub fn get(&self, key: &String) -> Option<String> {
+        self.hash_map.get(key).cloned()
     }
 }
 
@@ -30,7 +36,7 @@ mod tests {
     use crate::database::Database;
 
     #[test]
-    fn test_put() {
+    fn test_database_and_backing_storage() {
         let dir = TempDir::new("test").unwrap();
         let file_path = dir.path().join("test.db");
         let path = file_path.to_str().unwrap().to_string();
@@ -40,6 +46,16 @@ mod tests {
         db.put("key".to_string(), "value3".to_string());
 
         // test that insert called twice with the same key overwrites the value
-        assert_eq!(db.get("key".to_string()).unwrap(), "value3".to_string());
+        assert_eq!(db.get(&"key".to_string()).unwrap(), "value3".to_string());
+        let path = file_path.to_str().unwrap().to_string();
+        let db2 = Database::new(path);
+        assert_eq!(db2.get(&"key".to_string()).unwrap(), "value3".to_string());
+        
+        db.put("key4".to_string(), "value4".to_string());
+        assert_eq!(db.get(&"key4".to_string()).unwrap(), "value4".to_string());
+
+        let path = file_path.to_str().unwrap().to_string();
+        let db2 = Database::new(path);
+        assert_eq!(db2.get(&"key4".to_string()).unwrap(), "value4".to_string());
     }
 }
--- toydb/src/main.rs
+++ toydb/src/main.rs
@@ -1,9 +1,64 @@
+use axum::{
+    extract::{Path, State},
+    routing::{get, post},
+    Router,
+    response::{IntoResponse, Response},
+    http::StatusCode,
+};
+use tokio::sync::RwLock;
+use std::sync::Arc;
 use crate::database::Database;
 
 mod persistence;
 mod database;
 
-fn main() {
+type Store = Arc<RwLock<Database>>;
 
-    let db = Database::new("test.db".to_string());
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let store: Store = Arc::new(RwLock::new(Database::new("test.db".to_string())));
+
+    let app = Router::new()
+        .route("/set/{k}/{v}", post(store_set_value))
+        .route("/get/{k}", get(store_get_value))
+        .with_state(store);
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// -------- handlers ----------
+#[derive(Debug)]
+enum GetResult {
+    Found(String),
+    NotFound,
+}
+
+impl IntoResponse for GetResult {
+    fn into_response(self) -> Response {
+        match self {
+            GetResult::Found(val) => val.into_response(), // 200 OK by default
+            GetResult::NotFound => StatusCode::NOT_FOUND.into_response(),
+        }
+    }
+}
+
+async fn store_set_value(
+    State(store): State<Store>,
+    Path((k, v)): Path<(String, String)>,
+) {
+    store.write().await.put(k, v);
+}
+
+async fn store_get_value(
+    State(store): State<Store>,
+    Path(k): Path<String>,
+) -> GetResult {
+    let guard = store.read().await;
+    match guard.get(&k) {
+        Some(value) => GetResult::Found(value.clone()),
+        None => GetResult::NotFound,
+    }
 }
